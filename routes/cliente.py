484149efc1082ecc8e16dfafb39673ba3b232153
israel <zdesenhos@gmail.com>
2025-05-01 04:37:03 -0300

    post efetuado com sucesso. Criação de novos funcionarios operante. Falta a função delete

--- routes/cliente.py
+++ routes/cliente.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from database.cliente import CLIENTES
 
 cliente_rout = Blueprint('cliente', __name__)
@@ -23,7 +23,17 @@ def lista_clientes():
 # inserir os dados do cliente no servidor
 @cliente_rout.route('/', methods=['POST'])
 def inserir_cliente():
-    pass
+    data = request.json
+
+    novo_usuario = {
+        'id': len(CLIENTES) + 1,
+        'nome': data['nome'],
+        'email': data['email']
+    }
+
+    CLIENTES.append(novo_usuario)
+
+    return render_template('item_cliente.html', cliente=novo_usuario)
 
 # Form para cadastro de cliente
 @cliente_rout.route('/new')
